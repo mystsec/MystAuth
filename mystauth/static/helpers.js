7bbcfa688859579d8306624c8c7d2cefc4a13925
mystsec <122738547+mystsec@users.noreply.github.com>
2024-02-24 14:27:12 -0800

    Create helpers.js
    Centralizes helper functions for the auth/login portal pages

---
+++ mystauth/static/helpers.js
@@ -0,0 +1,77 @@
+async function checkSupport() {
+  let bioOnly = document.getElementById("bioOnly").innerHTML == "True";
+  if (window.PublicKeyCredential) {
+    if (!bioOnly)
+    {
+      return true;
+    }
+    else
+    {
+      let plat = await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()
+      if (plat) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  else {
+    return false;
+  }
+}
+
+function checkCookieSupport() {
+  return navigator.cookieEnabled;
+}
+
+function notify(str, type) {
+  let notif = document.getElementById('notif');
+  if (type == 0)
+  {
+    notif.setAttribute('success', 'false');
+  }
+  else
+  {
+    notif.setAttribute('success', 'true');
+  }
+  notif.innerHTML = str;
+}
+
+function noPasskeySupport()
+{
+  document.getElementById("loading_msg").innerHTML = "Your Device/Browser Doesn't Support <a href='https://blog.google/inside-google/googlers/ask-a-techspert/how-passkeys-work/' target='_blank'>Passkeys</a> 😢 <br><br><a href='https://passkeys.dev/device-support/#matrix' target='_blank'>Check Support</a>";
+  loading();
+}
+
+function noCookieSupport()
+{
+  document.getElementById("loading_msg").innerHTML = "Please Enable Cookies <br><br> <a href='https://mystauth.com/privacy/#kix.sbkz6l6dox82' target='_blank'>Only used to Identify and Secure Account</a>";
+  loading();
+}
+
+function onlySpaces(str)
+{
+  return str.replace(/\s/g, '').length == 0;
+}
+
+function prepURL(url)
+{
+  url = decodeURIComponent(url)
+  return url.replace(/"/g, "%22").replace(/'/g, "%27").replace(/</g, "%3C").replace(/>/g, "%3E")
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
