eb91ed8acc55f2b88dcb9addecc92a5b8b81db1e
mystsec <122738547+mystsec@users.noreply.github.com>
2024-02-24 15:59:12 -0800

    Update settings.py
    Set signing keys from .env file

--- myst/settings.py
+++ myst/settings.py
@@ -15,6 +15,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+JWT_SIG_KEYS = env.list("SIG_KEYS")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
